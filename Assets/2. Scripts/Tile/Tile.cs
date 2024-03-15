cf525ea19f5d31ae103ec7198f42997444f1898d
leejy811 <lee38956162@gmail.com>
2024-03-15 13:41:05 +0900

    Add Influence Value

--- Assets/2. Scripts/Tile/Tile.cs
+++ Assets/2. Scripts/Tile/Tile.cs
@@ -1,8 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public enum TileType { Ground, Road, Decoration }
+
+[Serializable]
+public class InfluenceValue
+{
+    public BuildingType type;
+    public int value;
+
+    public InfluenceValue(BuildingType type, int value)
+    {
+        this.type = type;
+        this.value = value;
+    }
+
+    public bool CheckEqual(InfluenceValue inValue)
+    {
+        return type == inValue.type && value == inValue.value;
+    }
+}
 
 public class Tile : MonoBehaviour
 {
@@ -10,6 +29,7 @@ public class Tile : MonoBehaviour
     public bool isPurchased { get; private set; }
     public int costPerDay { get; private set; }
     public GameObject building;
+    public List<InfluenceValue> influenceValues;
 
     public bool CheckBuilding()
     {
@@ -31,5 +51,14 @@ public class Tile : MonoBehaviour
             mat.color = Color.green;
         else
             mat.color = Color.red;
+    }
+
+    public void RemoveInfluence(InfluenceValue value)
+    {
+        for(int i = 0;i < influenceValues.Count; i++)
+        {
+            if (influenceValues[i].Equals(value))
+                influenceValues.RemoveAt(i);
+        }
     }
 }
