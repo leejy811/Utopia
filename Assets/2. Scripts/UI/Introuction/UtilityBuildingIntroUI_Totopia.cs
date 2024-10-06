7d538689d4dcc6d73abff4aa17f63efe442f8f08
leejy811 <lee38956162@gmail.com>
2024-10-07 03:32:43 +0900

    Add Totopia Utility Building Intro UI

---
+++ Assets/2. Scripts/UI/Introuction/UtilityBuildingIntroUI_Totopia.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UtilityBuildingIntroUI_Totopia : UtilityBuildingIntroUI
+{
+    [Header("Totopia")]
+    public GameObject minigamePanel;
+    public TextMeshProUGUI betChipText;
+    public TextMeshProUGUI betTimesText;
+    public Button gameButton;
+
+    public override void SetValue(Building building)
+    {
+        base.SetValue(building);
+
+        if (building.type == BuildingType.Culture)
+        {
+            minigamePanel.SetActive(true);
+
+            EnterBuilding enterBuilding = building as EnterBuilding;
+            betChipText.text = enterBuilding.betChip.ToString();
+            betTimesText.text = enterBuilding.betTimes.ToString();
+
+            if (enterBuilding.betTimes == 0)
+                gameButton.interactable = false;
+            else
+                gameButton.interactable = true;
+        }
+        else
+            minigamePanel.SetActive(false);
+    }
+}
